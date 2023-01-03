6dd16247e32902edbcfebd8aff6085eb137fb60f
henner espinoza <hennerespinoza@henners-MacBook-Air.local>
2023-01-03 17:45:59 -0500

    initial js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// creating initial variables including: start button, words (array of words), timer container, wins/losses 
+
+var startButton = document.getElementById("start");
+var words = [];
+var word = document.getElementById("word-text");
+var timer = document.getElementById("seconds")
