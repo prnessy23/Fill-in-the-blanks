e4fe9a9757f80ce29cb969c4c78f29c15f851ba3
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-01-03 19:14:56 -0800

    Add start game functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ startButton.addEventListener("click", start);
 
 function start(){
 startTimer();
-// startWords();
+startWords();
 }
 
 function startTimer(){
@@ -26,4 +26,27 @@ interval= setInterval(function(){
         endGame();
     }
 }, 1000); 
+}
+
+function startWords() {
+    if (words.length > 0) {
+        var currentWord = words.splice(Math.floor(Math.random() * words.length), 1)[0];
+        for (var i = 0; i < currentWord.length; i++) {
+            var newSpan = document.createElement("span");
+            newSpan.textContent = "_ ";
+            newSpan.setAttribute("id", "letter" + i);
+            word.appendChild(newSpan);
+            document.addEventListener("keydown", evalMatch);
+        }
+    } else {
+        endGame();
+    }
+}
+
+function endGame() {
+
+}
+
+function evalMatch() {
+    
 }
