59b18e87d5060d9200ea51528102916dc5f7f0d9
henner espinoza <hennerespinoza@henners-MacBook-Air.local>
2023-01-03 19:55:37 -0500

    timer feature added

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-// creating initial variables including: start button, words (array of words), timer container, wins/losses 
-
-var startButton = document.getElementById("start");
-var words = [];
-var word = document.getElementById("word-text");
-var timer = document.getElementById("seconds")
